bc9c8606bdf9dc3e6097078441b631db57d8d978
AJIR <155471185+JPKEEFEAJ@users.noreply.github.com>
2024-09-19 13:34:47 -0400

    Update script.js

--- Quiz Thing/js/script.js
+++ Quiz Thing/js/script.js
@@ -1,26 +1,28 @@
-const count_container = document.getElementById("count-container"); 
-const skittles = document.getElementById("skittles"); 
+const count_container = document.getElementById("count-container");
+const skittles = document.getElementById("skittles");
 
 let COUNT_BTN = true,
-COLOR_BTN = 0;
+    COLOR_BTN = 0;
 
 function count() {
-    for(let i = 0; i < 257; i++) {
-        if(i == 0) {
-            count_container.innerHTML += `${(i+1).toString()}`;
-        } else {
-            count_container.innerHTML += `, ${i+1}`;
+    if (COUNT_BTN) {
+        for (let i = 0; i < 257; i++) {
+            if (i == 0) {
+                count_container.innerHTML += `${(i + 1).toString()}`;
+            } else {
+                count_container.innerHTML += `, ${i + 1}`;
+            }
         }
     }
     COUNT_BTN = false;
 }
 
 function color() {
-    if(COLOR_BTN == 0) {
+    if (COLOR_BTN == 0) {
         skittles.style.color = "purple";
-    } else if(COLOR_BTN == 1) {
+    } else if (COLOR_BTN == 1) {
         skittles.style.color = "red";
-    } else if(COLOR_BTN == 2) {
+    } else if (COLOR_BTN == 2) {
         skittles.style.color = "green";
     }
     COLOR_BTN++;
