33bc1368bc86969b9c6fdb2397128d2ad7961f90
AJIR <155471185+JPKEEFEAJ@users.noreply.github.com>
2024-09-19 12:27:05 -0400

    Add files via upload

---
+++ Quiz Thing/js/script.js
@@ -0,0 +1,39 @@
+const count_container = document.getElementById("count-container"); 
+const skittles = document.getElementById("skittles"); 
+
+let COUNT_BTN = true,
+COLOR_BTN = 0;
+
+function count() {
+    for(let i = 0; i < 256; i++) {
+        if(i == 0) {
+            count_container.innerHTML += `${(i+1).toString()}`;
+        } else {
+            count_container.innerHTML += `, ${i+1}`;
+        }
+    }
+    COUNT_BTN = false;
+}
+
+function color() {
+    if(COLOR_BTN == 0) {
+        skittles.style.color = "purple";
+    } else if(COLOR_BTN == 1) {
+        skittles.style.color = "red";
+    } else if(COLOR_BTN == 2) {
+        skittles.style.color = "green";
+    }
+    COLOR_BTN++;
+}
+
+function reset() {
+    //Buttons
+    COUNT_BTN = true;
+    COLOR_BTN = 0;
+
+    //Skittles
+    skittles.style.color = "black";
+
+    //Clear
+    count_container.innerHTML = "";
+}
